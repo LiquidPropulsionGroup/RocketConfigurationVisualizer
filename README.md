23819846aedf94ff042b69c845ec67de32b96e9c
Vertycoxe <=>
2020-07-23 22:12:00 -0700

    Lambda functions work, edited readme

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
 1. Install python (It can be the newest one)
 2. Pull a clone of this repository
 3. Create a python virtual enviorment with the following command in the root directory of the Rocket Configuration Visualizer. `python -m venv venv/`
-4. Finally install all the dependencys once in your virtual enviorment of python with `pip3 install -r requirements.txt`
+4. Finally install all the dependencys once in your virtual enviorment of python with `pip3 install -r requirements.txt` (do this in the Terminal of VS Code, not cmd)
--- chemistry.py
+++ chemistry.py
@@ -146,17 +146,17 @@ def get_mdot() -> float:
 
     return mdot
 
-
 def getPoints(endpoints, r1=0.05, convergence_angle=30, r2=0.03,r3=0.025,step=1e-6):
 
     functions = [
         lambda x: endpoints[0][1],  # straight line
-        lambda x: np.sqrt(abs(r1 ** 2 - (endpoints[1][1] - x) ** 2)) + endpoints[1][1] - r1,  # circle
+        #lambda x: np.sqrt(abs(r1 ** 2 - (endpoints[1][1] - x) ** 2)) + endpoints[1][1] - r1,  # circle
+        lambda x: np.sqrt(r1**2 - (x - endpoints[1][0]) ** 2) + endpoints[1][1] - r1,
         lambda x: -np.pi * ((convergence_angle/180 )* (x - endpoints[2][0])) + endpoints[2][1],  # straight line
-        lambda x: -np.sqrt(abs(r2 ** 2 - (endpoints[3][1] - x) ** 2)) + endpoints[3][1] - r2,  # circle
-        lambda x: -np.sqrt(abs(r3 ** 2 - (endpoints[4][1] - x) ** 2)) + endpoints[4][1] - r3,  # circle
-        lambda x: ((endpoints[6][1] - endpoints[5][1]) / (endpoints[6][0] - endpoints[5][0])) * (x - endpoints[2][0]) +
-                  endpoints[2][1]  # straight line
+        lambda x: -np.sqrt(r2 ** 2 - (x - endpoints[4][0]) ** 2) + endpoints[4][1] + r2,  # circle
+        lambda x: -np.sqrt(r3 ** 2 - (x - endpoints[4][0]) ** 2) + endpoints[4][1] + r3,  # circle
+        lambda x: ((endpoints[5][1] - endpoints[6][1]) / (endpoints[5][0] - endpoints[6][0])) * (x - endpoints[5][0]) +
+                  endpoints[5][1]  # straight line
     ]
     
     num = np.int32(np.rint((endpoints[6][0] - endpoints[0][0]) / step))
@@ -171,13 +171,16 @@ def getPoints(endpoints, r1=0.05, convergence_angle=30, r2=0.03,r3=0.025,step=1e
         x = np.append(x, temp_x)
     return x, y
 
-x, y = getPoints(np.array(
+                       
+# These are just some hardcoded test values
+x,y = getPoints(np.array(
     [[-0.190432417470023, 0.0400000000000000], 
      [-0.0618912817675829, 0.0400000000000000],
      [-0.0368912817675829, 0.0333012701892219],
      [-0.0150000000000000, 0.0206623327678020],
      [0, 0.0166430948813352],
      [0.00586669938711653, 0.0173412052778248],
-     [0.0401400000000000, 0.0256163128987331]]), step=0.1)
+     [0.0401400000000000, 0.0256163128987331]]), step=0.00001)
+
 plt.plot(x, y)
 plt.show()
