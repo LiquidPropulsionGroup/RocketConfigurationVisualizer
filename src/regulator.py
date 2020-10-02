8a07111f8d0e4449715ff5043e00e203dbb379f4
Makar Loktyukhin <makar.loktyukhin@gmail.com>
2020-10-01 21:49:34 -0700

    Create regulator.py

---
+++ src/regulator.py
@@ -0,0 +1,17 @@
+import math
+#import matplotlib as plt
+import numpy as np
+#import scipy as sp
+
+# The purpose of this script is to calculate supply pressure effects in the plumbing system
+
+fuel_volFlow = 3.015427325 # gpm
+lox_volFlow = 4.778712667 # gpm
+
+He_init_press = 1800 #psig
+He_vol = 2990 # in^3
+He_init_temp = 303 # K
+
+fuel_volEmpty = 360.04009 # in^3
+lox_volEmpty = 360.04009 # in^3
+
