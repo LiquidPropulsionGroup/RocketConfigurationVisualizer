cd6b7b443c4af72402721f70dd8702db755231e1
Makar Loktyukhin <makar.loktyukhin@gmail.com>
2020-10-03 09:17:55 -0700

    helium thermophyical props and van der Waals

--- src/regulator.py
+++ src/regulator.py
@@ -5,6 +5,18 @@ import numpy as np
 
 # The purpose of this script is to calculate supply pressure effects in the plumbing system
 
+#   Helium thermophysical properties
+m = 4.0026 # g/mol
+cp = 5188 # J/kg
+gamma = 1.66
+Rbar = 2077 J/kg*K
+
+#   van der Waals correction factors
+a = 3.46e-3 # Pa * m^6
+b = 23.71e-6 # m^3/mol
+def vanderWaal( P, a, n, V, b, R ): #solving for T
+    
+
 fuel_volFlow = 3.015427325 # gpm
 lox_volFlow = 4.778712667 # gpm
 
