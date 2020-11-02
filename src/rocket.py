18fb7158b673fed9e01d299d396e3d89fb248593
Makar Loktyukhin <makar.loktyukhin@gmail.com>
2020-11-02 14:40:31 -0800

    Update rocket.py

--- src/rocket.py
+++ src/rocket.py
@@ -17,13 +17,6 @@ def hoop_stress(internal_pressure, inside_diameter, wall_thickness):
     hoopStress = internal_pressure * inside_diameter / 2 * wall_thickness
     return hoopStress
 
-def bartz(d_throat, p_chamber, c_star, d, c_p, visc, t_gas, t_wall):
-    """bartz equation calculator"""
-    t_boundary = (t_gas + t_wall) / 2
-    return (0.026 / math.pow(d_throat, 0.2) * math.pow((p_chamber / c_star), 0.8) * math.pow((d_throat / d),
-                                                                                             1.8) * c_p * math.pow(
-        visc, 0.2) * math.pow((t_gas / t_boundary), (0.8 - 0.2 * 0.6)))
-
 # chamber diameter(0.08m), lambda curve, 15degree nozzle.
 
 def nozzleAreaEquation(mdot, pres, temp, rbar, gam): 
@@ -31,7 +24,7 @@ def nozzleAreaEquation(mdot, pres, temp, rbar, gam):
             (1 + ((gam - 1) / 2)), ((gam + 1) / (2 * (gam - 1))))
 
 class Rocket:
-    def __init__(self, chem, mdot, l_star, cham_d, conv_angle, div_angle):
+    def __init__(self, chem, mdot, l_star, cham_d, conv_angle = 30, div_angle = 15, r1=0.05, r2=0.03, r3=0.025):
         self.inj = chem[0] # injector
         self.cham = chem[1] # converging starts (end of chamber)
         self.thr = chem[2] # throat
@@ -49,6 +42,10 @@ class Rocket:
         self.hoopStress_arr = None
         self.conv_angle = conv_angle
         self.divergence_angle = div_angle
+        self.r1 = r1
+        self.r2 = r2
+        self.r3 = r3
+        self.Cstar = self.cham.p * self.thr.a / self.mdot
         #self.props = 
 
         # Specific impulse in seconds
@@ -76,39 +73,40 @@ class Rocket:
         self.areas()
         self.areaMach()
         self.tempPressureDensity()
+        self.cpCurveFit()
 
     # this generates the points that the gencontour function uses to make functions between
     # the points are referenced from left to right in the graph
-    def my_contourPoints(self, r1=0.05, r2=0.03, r3=0.025):
+    def my_contourPoints(self):
         o = [0,self.thr.d / 2]
-        d = [-r2 * np.sin(self.conv_angle),o[1] + r2 * (1 - np.cos(self.conv_angle))]
-        n = [r3 * np.sin(self.divergence_angle), o[1] + r3 * np.sin(1 - np.cos(self.divergence_angle))]
+        d = [-self.r2 * np.sin(self.conv_angle),o[1] + self.r2 * (1 - np.cos(self.conv_angle))]
+        n = [self.r3 * np.sin(self.divergence_angle), o[1] + self.r3 * np.sin(1 - np.cos(self.divergence_angle))]
         e = [n[0] + ((self.exit.d / 2) - n[1]) * np.sin(math.pi/2 - self.divergence_angle)/np.sin(self.divergence_angle), self.exit.d / 2]
         a = [None, self.cham.d / 2]
         b = [None, self.cham.d / 2]
-        c = [None,self.cham.d / 2 - (r1 * (1 - np.cos(self.conv_angle)))]
+        c = [None,self.cham.d / 2 - (self.r1 * (1 - np.cos(self.conv_angle)))]
         c[0] = d[0] - (c[1] - d[1]) * (np.sin(math.pi/2 - self.conv_angle)/np.sin(self.conv_angle))
-        b[0] = c[0] - (r1 * np.sin(self.conv_angle))
+        b[0] = c[0] - (self.r1 * np.sin(self.conv_angle))
         a[0] = b[0] - self.chamber_length
         self.contourPoints = [a, b, c, d, o, n, e]
 
-    def genContour(self, r1=0.05, convergence_angle=30, r2=0.03, r3=0.025, step=1e-4): 
+    def genContour(self, step=1e-4): 
         # This is the function that draws the discrete contour
         # these functions are referenced from left to right of the graph
         functions = [
             lambda x: self.contourPoints[0][1],
-            lambda x: np.sqrt(r1 ** 2 - (x - self.contourPoints[1][0]) ** 2) + self.contourPoints[1][1] - r1,
+            lambda x: np.sqrt(self.r1 ** 2 - (x - self.contourPoints[1][0]) ** 2) + self.contourPoints[1][1] - self.r1,
             lambda x: ((self.contourPoints[2][1] - self.contourPoints[3][1]) / (self.contourPoints[2][0] - self.contourPoints[3][0]))  * (x - self.contourPoints[2][0]) + self.contourPoints[2][1],
-            lambda x: -np.sqrt(r2 ** 2 - (x - self.contourPoints[4][0]) ** 2) + self.contourPoints[4][1] + r2, 
-            lambda x: -np.sqrt(r3 ** 2 - (x + self.contourPoints[4][0]) ** 2) + self.contourPoints[4][1] + r3, 
+            lambda x: -np.sqrt(self.r2 ** 2 - (x - self.contourPoints[4][0]) ** 2) + self.contourPoints[4][1] + self.r2, 
+            lambda x: -np.sqrt(self.r3 ** 2 - (x + self.contourPoints[4][0]) ** 2) + self.contourPoints[4][1] + self.r3, 
             lambda x: ((self.contourPoints[5][1] - self.contourPoints[6][1]) / (self.contourPoints[5][0] - self.contourPoints[6][0]))  * (x - self.contourPoints[5][0]) + self.contourPoints[5][1] 
         ]
-            # 1: strait line
+            # 1: straight line
             # 2: circle
-            # 3: stait line
+            # 3: staight line
             # 4: circle
             # 5: circle
-            # 6: stait line
+            # 6: staight line
 
         num = np.int32(np.rint((self.contourPoints[6][0] - self.contourPoints[0][0]) / step))
 
@@ -209,8 +207,46 @@ class Rocket:
             self.density_arr[1,count] = self.density_eq(mach)
             count += 1
 
+    def cpCurveFit(self):
         
-    def hoopStress(self): #work in progress
-        #hoopStress = internal_pressure * inside_diameter / 2 * wall_thickness
-        self.hoopStress_arr = self.contour.copy()
-        self.hoopStress_arr = np.multiply(self.hoopStress_arr,)#needs pressure at every point
+        #self.contourPoints = [a, b, c, d, o, n, e]
+        points = [self.contourPoints[1], self.contourPoints[4], self.contourPoints[6]]
+        cp_set = [self.cham.cp, self.thr.cp, self.exit.cp]
+
+        pars, cov = sp.optimize.curve_fit(f = power_law, xdata = points, ydata = cp_set, p0 = [0, 0], bounds = (-np.inf, np.inf) )
+        print(pars)
+        print(cov)
+
+
+        '''self.contourPoints
+        lambda x: m*x + b
+        lambda x: pars[0]*x**pars[1]
+        self.cpCurve = self.contour.copy()'''
+
+
+    def bartz(self):
+        
+        return
+        
+'''   
+def bartz(d_throat, p_chamber, c_star, d, c_p, visc, t_gas, t_wall):
+    """bartz equation calculator"""
+    t_boundary = (t_gas + t_wall) / 2
+    return (0.026 / math.pow(d_throat, 0.2) * math.pow((p_chamber / c_star), 0.8) * math.pow((d_throat / d),
+                                                                                             1.8) * c_p * math.pow(
+        visc, 0.2) * math.pow((t_gas / t_boundary), (0.8 - 0.2 * 0.6)))
+
+'''
+
+#needed veriables for bartz
+
+#throat diameter
+#throat to exit radius ratio
+#throat radius curvature
+#characteristic velocity
+#back pressure/ chamber pressure
+
+#Prandtl number
+#viscosity
+#Cp
+
