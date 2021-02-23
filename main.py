8758bf05ffeea61cb1e28453ebcb97d8e45df527
emmettrm <68245928+emmettrm@users.noreply.github.com>
2021-02-22 23:34:59 -0800

    random small changes

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ l_star = 1.1
 cham_d = 3.75 * 0.0254 #in meters
 conv_angle = math.pi / 4 # rad, 45deg
 div_angle = math.pi / 12  # rad, 15deg
-wall_temp = 1000 # K
+wall_temp = 500 # K
 r1 = 0.0254
 r2 = 0.0127
 r3 = 0.01905
 step = 5e-4
+propellant_ratio = 2.3
 stinger = Rocket(title, chems, mdot, l_star, cham_d, conv_angle, div_angle, wall_temp, r1, r2, r3, step)
 stinger.variablesDisplay()
 stinger.graphDisplay()
@@ -37,6 +38,7 @@ r1 = 0.0254
 r2 = 0.0127
 r3 = 0.01905
 step = 5e-4
+propellant_ratio = 2.3
 stinger = Rocket(title, chems, mdot, l_star, cham_d, conv_angle, div_angle, wall_temp, r1, r2, r3, step)
 #stinger.variablesDisplay()
 #stinger.graphDisplay()
@@ -44,9 +46,9 @@ stinger = Rocket(title, chems, mdot, l_star, cham_d, conv_angle, div_angle, wall
 #methate engine test
 title = 'Methane Engine Test'
 chems = Chemistry.parse_initVeriables('methane_test_50bar')
-mdot = 10
+mdot = 20
 l_star = 1.1
-cham_d = 5.5 * 0.0254 #in meters
+cham_d = 8 * 0.0254 #in meters
 conv_angle = math.pi / 4 # rad, 45deg
 div_angle = math.pi / 8  # rad, 15deg
 wall_temp = 1000 # K
@@ -54,6 +56,7 @@ r1 = 0.0254
 r2 = 0.0127
 r3 = 0.01905
 step = 5e-4
+propellant_ratio = 2.7
 methane = Rocket(title, chems, mdot, l_star, cham_d, conv_angle, div_angle, wall_temp, r1, r2, r3, step)
 #methane.variablesDisplay()
 #methane.graphDisplay()
