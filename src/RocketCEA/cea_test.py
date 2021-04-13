ea48eb5a88f977ce53942c2063797ab7d6a25705
emmettrm <68245928+emmettrm@users.noreply.github.com>
2021-04-12 17:48:40 -0700

    cea changes

--- src/RocketCEA/cea_test.py
+++ src/RocketCEA/cea_test.py
@@ -1,4 +1,4 @@
 #from RocketCEA.rocketcea import *
-from RocketCEA.rocketcea.cea_obj import CEA_Obj
+from rocketcea.cea_obj import CEA_Obj
 myC = CEA_Obj(oxName = 'LOX', fuelName = 'LH2')
 print(myC.get_Isp(Pc = 100.0, MR = 1.0, eps = 40.0))
